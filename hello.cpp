b78a03f2a68c010a9dfa43fb04b145eaad1a7665
Halalisanin <Halalisanin@gmail.com>
2024-03-13 06:45:59 +0200

    hello

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "Hello, World!" followed by new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+    std::cout << "Hello, World!\n"; /** or std::cout << "Hello, World!" << std:: endl; */
+
+    return (0);
+
+    }
